fe9e1d01f85ae40b7eb9f4742657fb0338da6e70
胡林 <2352776704@qq.com>
2023-05-08 16:10:15 +0800

    2

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,7 @@ export default {
     description: '@huyuchen/visual',
     ignoreDeadLinks: true, // VitePress 在构建时不会因为死链接而导致构建失败。
     logo: '/login.jpg',
-    base: './',
+    base: './', // 使用相对路径
     footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright © 2019-present Evan You',
