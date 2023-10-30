33ebf39fd5449dff5b1f420db512e54f082ae8b7
hulin <2352776704@qq.com>
2023-10-30 10:02:06 +0800

    2

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,11 @@
 import DefaultTheme from "vitepress/theme"
 import HuUi from "@huyuchen/visual"
 import "@huyuchen/visual/dist/es/style.css"
-// import 'element-plus/dist/index.css'
+import "element-plus/dist/index.css"
 import "vue-virtual-scroller/dist/vue-virtual-scroller.css" // vitepress，组件库引用样式打包报错问题
-import "element-plus/theme-chalk/base.css"
-import "element-plus/theme-chalk/el-col.css"
-import "element-plus/theme-chalk/el-row.css"
+// import "element-plus/theme-chalk/base.css"
+// import "element-plus/theme-chalk/el-col.css"
+// import "element-plus/theme-chalk/el-row.css"
 import naiveUi from "naive-ui"
 
 export default {
