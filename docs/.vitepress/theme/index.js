0b5652cc06d73ed64c91835624e30bfe4aa0becf
hulin <2352776704@qq.com>
2024-12-20 14:51:18 +0800

    1

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,8 @@
 import DefaultTheme from "vitepress/theme"
 import HuUi from "@huyuchen/visual"
 import "@huyuchen/visual/dist/es/style.css"
-// import HuEditor from "@huyuchen/editor"
-// import "@huyuchen/editor/dist/es/style.css" // 引入样式
+import HuEditor from "@huyuchen/editor"
+import "@huyuchen/editor/dist/es/style.css" // 引入样式
 import naiveUi from "naive-ui"
 
 export default {
@@ -10,6 +10,6 @@ export default {
   async enhanceApp({ app }) {
     app.use(naiveUi)
     app.use(HuUi)
-    // app.use(HuEditor)
+    app.use(HuEditor)
   },
 }
